2ac82e2026c8ea529ed47155a1f6cdd59b65ff3f
lawwrites <lorraineawheat@gmail.com>
2025-05-21 17:58:11 -0700

    practiced coding

--- python_practice/py_practice.py
+++ python_practice/py_practice.py
@@ -115,42 +115,114 @@
 # activate = cleanList(raw_movies)
 # print(activate)
 
-runtimes = [95, 110, 87, 130, 100, 145, 98]
-above = [ x for x in runtimes if x > 100]
-print(above)
+# runtimes = [95, 110, 87, 130, 100, 145, 98]
+# above = [ x for x in runtimes if x > 100]
+# print(above)
 
-subs = {'Netflix': 232, 'Disney+': 157, 'Hulu': 50, 'Apple TV+': 25}
-new_sub = {key: sub for key, sub in subs.items() if sub > 100}
+# subs = {'Netflix': 232, 'Disney+': 157, 'Hulu': 50, 'Apple TV+': 25}
+# new_sub = {key: sub for key, sub in subs.items() if sub > 100}
 
-print(new_sub)
+# print(new_sub)
 
-platforms = ['Netflix', 'Hulu', 'Disney+']
-ratings = [4.8, 3.9, 4.5]
+# platforms = ['Netflix', 'Hulu', 'Disney+']
+# ratings = [4.8, 3.9, 4.5]
 
-zipit = zip(platforms, ratings)
-lz = list(zipit)
-print(lz)
+# zipit = zip(platforms, ratings)
+# lz = list(zipit)
+# print(lz)
 
-for key, value in lz:
-    print(f"{key}: {value}\n")
+# for key, value in lz:
+#     print(f"{key}: {value}\n")
 
-def show_genres(**kwargs):
-    for show, genre in kwargs.items():
-        print(f"{show}: {genre}")
+# def show_genres(**kwargs):
+#     for show, genre in kwargs.items():
+#         print(f"{show}: {genre}")
 
-test = {"Mulan": 8.5, "Little Mermaid": 5.5, "Zeus": 10}
-#or show_genres(Mulan="action", Mermaid="Fantasy")
+# test = {"Mulan": 8.5, "Little Mermaid": 5.5, "Zeus": 10}
+# #or show_genres(Mulan="action", Mermaid="Fantasy")
 
-printIT = show_genres(**test)
-print(printIT)
+# printIT = show_genres(**test)
+# print(printIT)
 
-class Movie:
-    def __init__(self, title, rating):
-        self.title = title
-        self.rating = rating
+# class Movie:
+#     def __init__(self, title, rating):
+#         self.title = title
+#         self.rating = rating
     
-    def is_hit(self):
-        if self.rating > 7.5:
+#     def is_hit(self):
+#         if self.rating > 7.5:
+#             return True
+#         else:
+#             return False
+
+
+platforms = {"Netflix": 230, "Hulu": 45, "Disney+": 110}
+
+reverses = {value: key for key, value in platforms.items() }
+print(reverses)
+
+def platform_summary(**kwargs):
+    new_dict = {platform for platform, sub in kwargs.items() 
+                if sub > 100}
+    to_lst = list(new_dict)
+    return to_lst
+ps = {"Netflix":230, "Hulu":45, "DisneyPlus":110}
+
+test = platform_summary(**ps)
+print(test)
+
+
+titles = ["Barbie", "Dune", "Oppenheimer"]
+ratings = [7.6, 8.2, 8.6]
+
+zipit = zip(titles, ratings)
+createD = dict(zipit)
+print(createD)
+
+class StreamingReport():
+    def __init__(self, platform, subscribers, avg_watch_time):
+        self.platform = platform
+        self.subscribers = subscribers
+        self.avg_watch_time = avg_watch_time
+
+    def is_popular(self):
+        if self.subscribers > 100:
             return True
         else:
             return False
+    
+num = StreamingReport("Paramount+", 20, 30)
+get_pop = num.is_popular()
+print(get_pop)
+
+import csv
+import pandas as pd
+
+def read_ratings():
+    df = pd.read_csv('ratings.csv')
+    df = df.dropna()
+    return df['score'].mean()
+
+
+class UserCleaner():
+    def __init__(self, filepath, df):
+        self.filepath = filepath
+        self.df = None
+    
+    def drop_blanks(self):
+        try:
+            self.df = pd.read_csv(self.filepath)
+            self.df = self.df.dropna()
+        except Exception as e:
+            with open("user_cleaning_errors.txt", "a") as log:
+                log.write("Error {self.filepath}: {e}\n")
+
+
+    def save_cleaned(self):
+       try:
+           self.df.to_csv("cleaned_users.csv", index=False)
+        except Exception as e:
+            with open("user_cleaning_errors.txt", "a") as log:
+                log.write("Error {self.filepath}: {e}\n")
+
+
